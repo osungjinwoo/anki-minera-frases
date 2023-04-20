21cd32219d264c36a6c086af26c14edf87c61112
michaelklaan <40411471+michaelklaan@users.noreply.github.com>
2023-04-20 03:41:33 -0300

    Update minera_stable.py

--- minera_stable.py
+++ minera_stable.py
@@ -41,7 +41,7 @@ def minerar_palavras():
         wordreference = "https://www.wordreference.com/definition/{}".format(f)
         linguee = "https://www.linguee.com.br/portugues-ingles/search?source=ingles&query={}".format(f)
         forvo = "https://forvo.com/word/{}/#en_usa".format(f)
-        cambridge = "https://dictionary.cambridge.org/pt/dicionario/ingles-portugues/{}".format(f)
+        cambridge = "https://dictionary.cambridge.org/dictionary/english-portuguese/{}".format(f)
         michaelis = "https://michaelis.uol.com.br/moderno-ingles/busca/ingles-portugues-moderno/{}".format(f)
         reverso = "https://www.reverso.net/tradu%C3%A7%C3%A3o-texto#sl=eng&tl=por&text={}".format(f)
         collins = "https://www.collinsdictionary.com/pt/dictionary/english/{}".format(f)
@@ -49,12 +49,16 @@ def minerar_palavras():
         oxford = "https://www.oxfordlearnersdictionaries.com/us/definition/english/{}".format(f)
         deepl = "https://www.deepl.com/translator#en/pt/{}".format(f)
         youglish = "https://pt.youglish.com/pronounce/{}/english/uk".format(f)
+        bab = "https://en.bab.la/dictionary/english-portuguese/{}".format(f)
+        pons = "https://en.pons.com/translate/english-portuguese/{}".format(f)
+        glosb = "https://glosbe.com/en/pt/{}".format(f)
+        rev = "https://conjugator.reverso.net/conjugation-english-verb-{}.html".format(f)
 
 
 #        webbrowser.open(forvo,new=2)
 #        webbrowser.open(linguee,new=2)
 #        webbrowser.open(michaelis,new=2)
-        webbrowser.open(oxford,new=2)
+        #webbrowser.open(oxford,new=2)
 #        webbrowser.open(reverso,new=2)
 #        webbrowser.open(googleimage,new=2)
         webbrowser.open(taoeba,new=2)
@@ -63,7 +67,10 @@ def minerar_palavras():
         webbrowser.open(cambridge,new=2)
 #        webbrowser.open(collins,new=2)
 #        webbrowser.open(youglish, new=2)
-
+        webbrowser.open(bab,new=2)
+        #webbrowser.open(pons,new=2)
+        webbrowser.open(glosb,new=2)
+        webbrowser.open(rev,new=2)
 
         # Pergunta se deseja continuar
         #resposta = input("deseja continuar minerando? (y/n)")
